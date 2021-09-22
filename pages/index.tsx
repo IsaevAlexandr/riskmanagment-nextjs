cfe73e7f7e4fa8e8cdb7ee1a1974bb69ed4cd1ff
IsaevAlexandr <IsaevAlexandr188@gmail.com>
2021-09-22 13:15:03 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { Layout } from "../components/Layout";
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <Layout>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Risks
+          </Typography>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Index;
